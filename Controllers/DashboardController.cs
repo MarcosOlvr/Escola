e0f2625841192a02729896234999f19a7d0c0e16
Marcos <marquinhos.dev@gmail.com>
2023-04-30 03:00:06 -0300

    modificando index do dashboard

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Escola.Models.ViewModels;
+using Escola.Repositories.Contracts;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Escola.Controllers
 {
     public class DashboardController : Controller
     {
+        private readonly IDashboardRepository _repo;
+
+        public DashboardController(IDashboardRepository repo)
+        {
+            _repo = repo;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var vm = new IndexDashboardVM();
+            vm.UltimasNotas = _repo.GetUltimasNotas();
+            vm.Materias = _repo.GetMaterias();
+            vm.AlunosNota = _repo.GetAlunosNota();
+
+            return View(vm);
         }
     }
 }
---
+++ Repositories/DashboardRepository.cs
@@ -0,0 +1,43 @@
+﻿using Escola.Data;
+using Escola.Models;
+using Escola.Repositories.Contracts;
+
+namespace Escola.Repositories
+{
+    public class DashboardRepository : IDashboardRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public DashboardRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public List<ApplicationUser> GetAlunosNota()
+        {
+            List<ApplicationUser> alunos = new List<ApplicationUser>();
+
+            var ultimasNotas = GetUltimasNotas();
+
+            foreach (var obj in ultimasNotas)
+            {
+                var aluno = _db.Users.FirstOrDefault(x => x.Id == obj.AlunoFK);
+
+                if (!alunos.Contains(aluno))
+                        alunos.Add(aluno);
+            }
+
+            return alunos;
+        }
+
+        public List<Materia> GetMaterias()
+        {
+            return _db.Materias.ToList().Take(5).ToList();
+        }
+
+        public List<Nota> GetUltimasNotas()
+        {
+            return _db.Notas.ToList().TakeLast(5).ToList();
+        }
+    }
+}
